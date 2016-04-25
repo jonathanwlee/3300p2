ab7c3e2a259aac4974a309a3cc2066d799cda46a
xcrliu <crl95@cornell.edu>
2016-04-25 01:58:15 -0400

    Added files via upload
    added 3rd visualization & dataset

---
+++ leicesterstats.json.js
@@ -0,0 +1,342 @@
+var leicester_data = [
+  {
+    "Week": 0,
+    "Date": "8/8/15",
+    "Match": "Leicester v Sunderland",
+    "Odds": 5000,
+    "Result": "4-2 (W)",
+    "ShotAccuracy": 53,
+    "Possession": 47,
+    "PassAccuracy": 74
+  },
+  {
+    "Week": 1,
+    "Date": "8/15/15",
+    "Match": "West Ham v Leicester",
+    "Odds": 5000,
+    "Result": "1-2 (W)",
+    "ShotAccuracy": 63,
+    "Possession": 40,
+    "PassAccuracy": 68
+  },
+  {
+    "Week": 2,
+    "Date": "8/22/15",
+    "Match": "Leicester v Tottenham",
+    "Odds": 3000,
+    "Result": "1-1 (D)",
+    "ShotAccuracy": 29,
+    "Possession": 43,
+    "PassAccuracy": 72
+  },
+  {
+    "Week": 3,
+    "Date": "8/29/15",
+    "Match": "Bournemouth v Leicester",
+    "Odds": 3000,
+    "Result": "1-1 (D)",
+    "ShotAccuracy": 50,
+    "Possession": 47,
+    "PassAccuracy": 68
+  },
+  {
+    "Week": 4,
+    "Date": "9/13/15",
+    "Match": "Leicester v Aston Villa",
+    "Odds": 3000,
+    "Result": "3-2 (W)",
+    "ShotAccuracy": 40,
+    "Possession": 55,
+    "PassAccuracy": 77
+  },
+  {
+    "Week": 5,
+    "Date": "9/19/15",
+    "Match": "Stoke v Leicester",
+    "Odds": 2500,
+    "Result": "2-2 (D)",
+    "ShotAccuracy": 45,
+    "Possession": 48,
+    "PassAccuracy": 73
+  },
+  {
+    "Week": 6,
+    "Date": "9/26/15",
+    "Match": "Leicester v Arsenal",
+    "Odds": 2500,
+    "Result": "2-5 (L)",
+    "ShotAccuracy": 54,
+    "Possession": 47,
+    "PassAccuracy": 79
+  },
+  {
+    "Week": 7,
+    "Date": "10/3/15",
+    "Match": "Norwich v Leicester",
+    "Odds": 2000,
+    "Result": "1-2 (W)",
+    "ShotAccuracy": 38,
+    "Possession": 42,
+    "PassAccuracy": 62
+  },
+  {
+    "Week": 8,
+    "Date": "10/17/15",
+    "Match": "Southampton v Leicester",
+    "Odds": 2000,
+    "Result": "2-2 (D)",
+    "ShotAccuracy": 37,
+    "Possession": 49,
+    "PassAccuracy": 73
+  },
+  {
+    "Week": 9,
+    "Date": "10/24/15",
+    "Match": "Leicester v C Palace",
+    "Odds": 2000,
+    "Result": "1-0 (W)",
+    "ShotAccuracy": 38,
+    "Possession": 50,
+    "PassAccuracy": 78
+  },
+  {
+    "Week": 10,
+    "Date": "10/31/15",
+    "Match": "West Brom v Leicester",
+    "Odds": 2000,
+    "Result": "2-3 (W)",
+    "ShotAccuracy": 63,
+    "Possession": 52,
+    "PassAccuracy": 71
+  },
+  {
+    "Week": 11,
+    "Date": "11/7/15",
+    "Match": "Leicester v Watford",
+    "Odds": 1000,
+    "Result": "2-1 (W)",
+    "ShotAccuracy": 60,
+    "Possession": 47,
+    "PassAccuracy": 70
+  },
+  {
+    "Week": 12,
+    "Date": "11/21/15",
+    "Match": "Newcastle v Leicester",
+    "Odds": 750,
+    "Result": "0-3 (W)",
+    "ShotAccuracy": 58,
+    "Possession": 48,
+    "PassAccuracy": 76
+  },
+  {
+    "Week": 13,
+    "Date": "11/28/15",
+    "Match": "Leicester v Man Utd",
+    "Odds": 350,
+    "Result": "1-1 (D)",
+    "ShotAccuracy": 60,
+    "Possession": 41,
+    "PassAccuracy": 66
+  },
+  {
+    "Week": 14,
+    "Date": "12/5/15",
+    "Match": "Swansea v Leicester",
+    "Odds": 100,
+    "Result": "0-3 (W)",
+    "ShotAccuracy": 50,
+    "Possession": 44,
+    "PassAccuracy": 72
+  },
+  {
+    "Week": 15,
+    "Date": "12/14/15",
+    "Match": "Leicester v Chelsea",
+    "Odds": 40,
+    "Result": "2-1 (W)",
+    "ShotAccuracy": 63,
+    "Possession": 41,
+    "PassAccuracy": 60
+  },
+  {
+    "Week": 16,
+    "Date": "12/19/15",
+    "Match": "Everton v Leicester",
+    "Odds": 33,
+    "Result": "2-3 (W)",
+    "ShotAccuracy": 50,
+    "Possession": 42,
+    "PassAccuracy": 63
+  },
+  {
+    "Week": 17,
+    "Date": "12/26/15",
+    "Match": "Liverpool v Leicester",
+    "Odds": 20,
+    "Result": "1-0 (L)",
+    "ShotAccuracy": 50,
+    "Possession": 42,
+    "PassAccuracy": 59
+  },
+  {
+    "Week": 18,
+    "Date": "12/29/15",
+    "Match": "Leicester v Man City",
+    "Odds": 16,
+    "Result": "0-0 (D)",
+    "ShotAccuracy": 36,
+    "Possession": 44,
+    "PassAccuracy": 63
+  },
+  {
+    "Week": 19,
+    "Date": "1/2/16",
+    "Match": "Leicester v Bournemouth",
+    "Odds": 18,
+    "Result": "0-0 (D)",
+    "ShotAccuracy": 14,
+    "Possession": 51,
+    "PassAccuracy": 79
+  },
+  {
+    "Week": 20,
+    "Date": "1/13/16",
+    "Match": "Tottenham v Leicester",
+    "Odds": 28,
+    "Result": "0-1 (W)",
+    "ShotAccuracy": 40,
+    "Possession": 44,
+    "PassAccuracy": 64
+  },
+  {
+    "Week": 21,
+    "Date": "1/16/16",
+    "Match": "Aston Villa v Leicester",
+    "Odds": 14,
+    "Result": "1-1 (D)",
+    "ShotAccuracy": 50,
+    "Possession": 46,
+    "PassAccuracy": 72
+  },
+  {
+    "Week": 22,
+    "Date": "1/23/16",
+    "Match": "Leicester v Stoke",
+    "Odds": 16,
+    "Result": "3-0 (W)",
+    "ShotAccuracy": 42,
+    "Possession": 49,
+    "PassAccuracy": 72
+  },
+  {
+    "Week": 23,
+    "Date": "2/2/16",
+    "Match": "Leicester v Liverpool",
+    "Odds": 11,
+    "Result": "2-0 (W)",
+    "ShotAccuracy": 60,
+    "Possession": 44,
+    "PassAccuracy": 65
+  },
+  {
+    "Week": 24,
+    "Date": "2/6/16",
+    "Match": "Man City v Leicester",
+    "Odds": 5.5,
+    "Result": "1-3 (W)",
+    "ShotAccuracy": 58,
+    "Possession": 40,
+    "PassAccuracy": 67
+  },
+  {
+    "Week": 25,
+    "Date": "2/14/16",
+    "Match": "Arsenal v Leicester",
+    "Odds": 2.2,
+    "Result": "2-1 (L)",
+    "ShotAccuracy": 43,
+    "Possession": 37,
+    "PassAccuracy": 64
+  },
+  {
+    "Week": 26,
+    "Date": "2/27/16",
+    "Match": "Leicester v Norwich",
+    "Odds": 3.33,
+    "Result": "1-0 (W)",
+    "ShotAccuracy": 38,
+    "Possession": 53,
+    "PassAccuracy": 73
+  },
+  {
+    "Week": 27,
+    "Date": "3/1/16",
+    "Match": "Leicester v West Brom",
+    "Odds": 2.4,
+    "Result": "2-2 (D)",
+    "ShotAccuracy": 27,
+    "Possession": 58,
+    "PassAccuracy": 77
+  },
+  {
+    "Week": 28,
+    "Date": "3/5/16",
+    "Match": "Watford v Leicester",
+    "Odds": 1.75,
+    "Result": "0-1 (W)",
+    "ShotAccuracy": 50,
+    "Possession": 50,
+    "PassAccuracy": 76
+  },
+  {
+    "Week": 29,
+    "Date": "3/14/16",
+    "Match": "Leicester v Newcastle",
+    "Odds": 1.25,
+    "Result": "1-0 (W)",
+    "ShotAccuracy": 25,
+    "Possession": 50,
+    "PassAccuracy": 79
+  },
+  {
+    "Week": 30,
+    "Date": "3/19/16",
+    "Match": "C Palace v Leicester",
+    "Odds": 1,
+    "Result": "0-1 (W)",
+    "ShotAccuracy": 50,
+    "Possession": 47,
+    "PassAccuracy": 70
+  },
+  {
+    "Week": 31,
+    "Date": "4/3/16",
+    "Match": "Leicester v Southampton",
+    "Odds": 0.62,
+    "Result": "1-0 (W)",
+    "ShotAccuracy": 67,
+    "Possession": 48,
+    "PassAccuracy": 74
+  },
+  {
+    "Week": 32,
+    "Date": "4/10/16",
+    "Match": "Sunderland v Leicester",
+    "Odds": 0.36,
+    "Result": "0-2 (W)",
+    "ShotAccuracy": 50,
+    "Possession": 51,
+    "PassAccuracy": 73
+  },
+  {
+    "Week": 33,
+    "Date": "4/17/16",
+    "Match": "Leicester v West Ham",
+    "Odds": 0.25,
+    "Result": "2-2 (D)",
+    "ShotAccuracy": 33,
+    "Possession": 45,
+    "PassAccuracy": 70
+  }
+]
